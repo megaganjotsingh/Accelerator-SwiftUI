d1c43a1a7f23cd63fb71f334a3436f757b8d3dab
Gaganjot Singh <megaganjotsingh@gmail.com>
2024-06-04 18:33:52 +0530

    initial

---
+++ Accelerator-SwiftUI/Accelerator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Accelerator_SwiftUIApp.swift
+//  Accelerator-SwiftUI
+//
+//  Created by Gaganjot Singh on 04/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Accelerator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Accelerator-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Accelerator-SwiftUI
+//
+//  Created by Gaganjot Singh on 04/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
